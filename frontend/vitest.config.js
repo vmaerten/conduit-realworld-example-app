b732df46dbb205023070a4e1c096e671b7773d00
Valentin Maerten <maerten.valentin@gmail.com>
2023-08-20 11:17:51 +0200

    chore: add two test script

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "frontend/src/setupTests.js",
+    setupFiles: "src/setupTests.js",
     css: true,
   },
 });
